7aa36e17439c19463a5a2987bb36a53312e083d1
beatz-steph <otenku123456@gmail.com>
2019-10-08 11:38:36 +0100

    Updates

---
+++ precache-manifest.e160b0a15c1fe3bf1973e779dd1b99f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52a8f9d164cd167715025ce723ff0fbb",
+    "url": "/monsters-rolodex-2/index.html"
+  },
+  {
+    "revision": "04fd0109e05464057a5f",
+    "url": "/monsters-rolodex-2/static/css/main.e358a966.chunk.css"
+  },
+  {
+    "revision": "2f59a044f2c237895c09",
+    "url": "/monsters-rolodex-2/static/js/2.62dba149.chunk.js"
+  },
+  {
+    "revision": "04fd0109e05464057a5f",
+    "url": "/monsters-rolodex-2/static/js/main.c210f69e.chunk.js"
+  },
+  {
+    "revision": "2be58b1f460e6fa88ed2",
+    "url": "/monsters-rolodex-2/static/js/runtime-main.a75661ad.js"
+  }
+]);
